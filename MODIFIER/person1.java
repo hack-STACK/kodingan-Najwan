48e20da87774662059f948e54912c6cb4547e2cc
M.Najwan hibatullah <60134671+hack-STACK@users.noreply.github.com>
2023-03-31 06:24:10 +0700

    Add files via upload

---
+++ MODIFIER/person1.java
@@ -0,0 +1,12 @@
+package MODIFIER;
+
+public class person1 
+{
+    public static void main(String[] args) 
+    {
+        person mPerson = new person();
+        mPerson.setName("najwan");
+
+        System.out.println("person name : " + mPerson.getName());
+    }
+}
