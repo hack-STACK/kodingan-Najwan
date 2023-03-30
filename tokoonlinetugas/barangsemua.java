9ad8f2b61de37e4e2054583fddfd85adc6b0ea9e
M.Najwan hibatullah <60134671+hack-STACK@users.noreply.github.com>
2023-03-31 06:23:01 +0700

    Delete tokoonlinetugas directory

--- tokoonlinetugas/barangsemua.java
+++
@@ -1,8 +0,0 @@
-package tokoonlinetugas;
-
-public class barangsemua extends barang1 {
-    barangsemua(int index,String namaBarang, int stokBarang, double hargaBarang, int penguranganStok) {
-        super(index,namaBarang, stokBarang, hargaBarang, penguranganStok);
-
-    }
-}
